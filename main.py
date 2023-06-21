667df8e426864e506d5c1de660e2a00277e49bc2
Nedislav Miladinov <nedy@Nedislavs-MacBook-Pro.local>
2023-06-21 15:40:10 +0300

    Add Poetry Dependencies

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from pathlib import Path
 from algo import dfs as d
 from generate import generate_folder_graph as g
 
+
 root_folder = Path(__file__).parent
 start = os.path.basename(root_folder)
 
 graph = g.generate_folder_graph(root_folder)
 
 d.dfs(graph, start)
+
+
